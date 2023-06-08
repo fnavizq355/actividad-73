6f1a0858b04ded11fea562f11d5a605a177079a8
fnavizq355 <132046114+fnavizq355@users.noreply.github.com>
2023-06-08 09:00:06 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
 basic.forever(function () {
-    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
         maqueen.motorStop(maqueen.Motors.M2)
-    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
         maqueen.motorStop(maqueen.Motors.M1)
     } else {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
