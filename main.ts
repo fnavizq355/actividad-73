a86fe90c66dabb8239fc6d73a0a5b435fda076f1
fnavizq355 <132046114+fnavizq355@users.noreply.github.com>
2023-06-08 08:58:15 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
 basic.forever(function () {
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
         maqueen.motorStop(maqueen.Motors.M2)
